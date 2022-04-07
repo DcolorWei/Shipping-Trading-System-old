c09fecad09ff95ab5767944d39879e300c0006ed
dcolor <corfer@yeah.net>
2022-04-08 00:36:42 +0800

    静态资源替换

--- src/assets/data/mattersList.ts
+++ src/assets/data/mattersList.ts
@@ -2,7 +2,7 @@ export default [
     {
         "id": "46",
         "client": {
-            "name": "在积便院",
+            "name": "大连零件制造",
             "phone": "19841428430",
             "email": "i.whfnip@qq.com",
             "facsimile": "ut"
@@ -33,25 +33,23 @@ export default [
         "statusList": [
             {
                 "time":"2022/2/23 14:00",
-                "name":"新增"
-            },
-            {
-                "time":"2022/2/23 14:00",
                 "name":"询价"
             }
         ],
         "siteList": [],
-        "cargoList": [],
+        "cargoList": [
+            
+        ],
         "cargoInfo": {
             "shipping": {
-                "country": "sit in",
-                "city": "重庆市",
-                "address": "黑龙江省海外-"
+                "country": "中华人民共和国",
+                "city": "大连市",
+                "address": "甘井子区大连零件制造厂"
             },
             "receiving": {
-                "country": "ad dolore labore Ut sunt",
-                "city": "重庆市",
-                "address": "海南省鹤岗市巴林左旗"
+                "country": "中华人民共和国",
+                "city": "高雄市",
+                "address": "高雄路425号"
             },
             "INCOTERMS": "amet aliquip ut anim",
             "specialneeds": {
@@ -108,113 +106,4 @@ export default [
         }
 
     },
-    {
-        "id": "390",
-        "client": {
-            "name": "在积便院",
-            "phone": "19841428430",
-            "email": "i.whfnip@qq.com",
-            "facsimile": "ut"
-        },
-        "carrier": {
-            "land": {
-                "landCompany": "",
-                "truckList": []
-            },
-            "shipagent": {
-                "id": "",
-                "name": ""
-            },
-            "sea": {
-                "shipCompany": "",
-                "shipName": "",
-                "sailingdate": "",
-                "voyage": "",
-                "passPorts": [],
-                "crewList": []
-            },
-            "box": {
-                "packingList": [],
-                "supervisingList": [],
-                "loadingList": []
-            }
-        },
-        "statusList": [
-            {
-                "time":"2022/2/23 14:00",
-                "name":"新增"
-            },
-            {
-                "time":"2022/2/23 14:00",
-                "name":"询价"
-            }
-        ],
-        "siteList": [],
-        "cargoList": [],
-        "cargoInfo": {
-            "shipping": {
-                "country": "sit in",
-                "city": "重庆市",
-                "address": "黑龙江省海外-"
-            },
-            "receiving": {
-                "country": "ad dolore labore Ut sunt",
-                "city": "重庆市",
-                "address": "海南省鹤岗市巴林左旗"
-            },
-            "INCOTERMS": "amet aliquip ut anim",
-            "specialneeds": {
-                "unstackable": false,
-                "perishable": false,
-                "clearance": false,
-                "dangerous": true,
-                "other": "esse occaecat laboris"
-            },
-            "deliverytime": "2022-02-01 00:00:00",
-            "duration": 858
-        },
-        "Inquiry": {
-            "shippingSchedule": [
-                {
-                    "ssid": "17",
-                    "passPorts": [
-                        "incididunt",
-                        "laborum in ut",
-                        "pariatur quis"
-                    ],
-                    "sailingdate": "1984-09-08 08:42:19",
-                    "voyage": 23,
-                    "shipCompany": "anim consectetur",
-                    "shipName": "红调论",
-                    "price": 14
-                },
-                {
-                    "ssid": "81",
-                    "passPorts": [
-                        "in sit minim",
-                        "nisi culpa ex laboris",
-                        "sunt cupidatat id consectetur"
-                    ],
-                    "sailingdate": "1989-06-17 06:24:43",
-                    "voyage": 80,
-                    "shipCompany": "velit",
-                    "shipName": "东群标全",
-                    "price": 27
-                }
-            ],
-            "landTransList": [
-                {
-                    "lsid": "58",
-                    "landCompany": "qui",
-                    "price": "59"
-                },
-                {
-                    "lsid": "41",
-                    "landCompany": "sed quis consectetur ea",
-                    "price": "36"
-                }
-            ]
-        }
-
-    }
 ]
